bca982d41c123a45646b1bda76c3ba6b0377d6e1
adi <96goldner@gmail.com>
2023-12-22 21:01:47 +0200

    more sepration into classes cellularAutomataViewer.py gameWindow.py

--- cellularAutomataViewer.py
+++ cellularAutomataViewer.py
@@ -9,12 +9,14 @@ class CellularAutomataViewer:
         self.width = width
         self.height = height
         self.cell_size = cell_size
+        self.parentFrame.grid_rowconfigure(0, weight=1)
+        self.parentFrame.grid_columnconfigure(0, weight=1)
         self.canvas = tk.Canvas(self.parentFrame, width=width * cell_size, height=height * cell_size, borderwidth=0,
                                 highlightthickness=0)
-        self.canvas.pack()
+        self.canvas.grid(row=0, column=0, sticky='nsew')
 
         self.average_temperature_label = tk.Label(self.parentFrame, text="Average Temperature: N/A")
-        self.average_temperature_label.pack()
+        # self.average_temperature_label.pack()
 
     def draw_board(self):
         self.canvas.delete("grid")
@@ -40,7 +42,7 @@ class CellularAutomataViewer:
                 # Draw the text at the center of the rectangle
                 # cloudsText = "C" if currCell.wind.clouds else "_"
                 self.canvas.create_text((x1 + x2) // 2, (y1 + y2) // 2, text=currCell.text,
-                                        font=("Arial", font_size, "bold"))
+                                        font=("Arial", font_size))
 
         # redraw average temperature
         self.average_temperature_label.config(text=f"Average Temperature: {self.cellularAutomata.average_temperature}")
--- gameWindow.py
+++ gameWindow.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 from cellularAutomata import CellularAutomata
 from cellularAutomataViewer import CellularAutomataViewer
 import time
@@ -12,8 +13,18 @@ class GameWindow:
         self.cell_size = cell_size
         self.window = tk.Tk()
         self.cellularAutomata = CellularAutomata(width, height)
-        self.cellularAutomataViewer = CellularAutomataViewer(self.window, self.cellularAutomata, width, height,
+
+        self.cellularAutomataFrame = tk.Frame(self.window)
+        self.window.columnconfigure(0, weight=1)
+        self.window.columnconfigure(1, weight=2)
+        self.window.rowconfigure(0, weight=1)
+        label1 = ttk.Label(self.window, text="label1", background="red")
+        label1.grid(row=0, column=0, sticky='nsew')
+        self.cellularAutomataViewer = CellularAutomataViewer(self.cellularAutomataFrame, self.cellularAutomata, width,
+                                                             height,
                                                              cell_size)
+
+        self.cellularAutomataFrame.grid(row=0, column=1, sticky='nsew')
 
     def start_game(self, generations=100):
         for _ in range(generations):
